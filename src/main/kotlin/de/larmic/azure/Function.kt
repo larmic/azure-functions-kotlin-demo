8842fe262934c6c76d088d7e78c033515aae1895
Lars Michaelis <mail@larmic.de>
2022-10-28 12:15:01 +0200

    simplify functions example

--- src/main/kotlin/de/larmic/azure/Function.kt
+++ src/main/kotlin/de/larmic/azure/Function.kt
@@ -8,27 +8,20 @@ class Function {
 
     @FunctionName("hello")
     fun run(
-        @HttpTrigger(name = "req",
+        @HttpTrigger(
+            name = "req",
             methods = [HttpMethod.GET],
             authLevel = AuthorizationLevel.ANONYMOUS
         ) request: HttpRequestMessage<Optional<String>>,
         context: ExecutionContext
     ): HttpResponseMessage {
-
         context.logger.info("HTTP trigger processed a ${request.httpMethod.name} request.")
 
-        val name = request.queryParameters["name"]
-
-        name?.let {
-            return request
-                .createResponseBuilder(HttpStatus.OK)
-                .body("Hi, $name!")
-                .build()
-        }
+        val name = request.queryParameters["name"] ?: "azure functions"
 
         return request
-            .createResponseBuilder(HttpStatus.BAD_REQUEST)
-            .body("Please pass a name on the query string or in the request body")
+            .createResponseBuilder(HttpStatus.OK)
+            .body("Hello $name!")
             .build()
     }
 
