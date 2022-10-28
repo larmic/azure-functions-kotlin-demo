3c383812e5ecf63e40fff37966c85e3f7431c92d
Lars Michaelis <mail@larmic.de>
2022-10-28 17:10:33 +0200

    replace assertj by kotest assertions

--- src/test/kotlin/de/larmic/azure/FunctionTest.kt
+++ src/test/kotlin/de/larmic/azure/FunctionTest.kt
@@ -1,9 +1,10 @@
 package de.larmic.azure
 
 import com.microsoft.azure.functions.*
+import de.larmic.azure.HttpResponseMessageMock.HttpResponseMessageBuilderMock
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
-import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.Optional
 import java.util.logging.Logger
@@ -14,7 +15,7 @@ class FunctionTest {
     private val azureFunction = Function()
 
     // request mock with default behavior
-    private val requestMock = mockk<HttpRequestMessage<Optional<String>>>() {
+    private val requestMock = mockk<HttpRequestMessage<Optional<String>>> {
         every { httpMethod } returns HttpMethod.GET
         every { createResponseBuilder(any()) } answers {
             firstArg<HttpStatus>().wrapByHttpResponseMessageBuilderMock()
@@ -32,8 +33,8 @@ class FunctionTest {
 
         val response = azureFunction.run(requestMock, executionContextMock)
 
-        assertThat(response.status).isEqualTo(HttpStatus.OK)
-        assertThat(response.body).isEqualTo("Hello azure functions!")
+        response.status shouldBe HttpStatus.OK
+        response.body shouldBe "Hello azure functions!"
     }
 
     @Test
@@ -42,9 +43,9 @@ class FunctionTest {
 
         val response = azureFunction.run(requestMock, executionContextMock)
 
-        assertThat(response.status).isEqualTo(HttpStatus.OK)
-        assertThat(response.body).isEqualTo("Hello larmic!")
+        response.status shouldBe HttpStatus.OK
+        response.body shouldBe "Hello larmic!"
     }
 
-    private fun HttpStatus.wrapByHttpResponseMessageBuilderMock() = HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(this)
+    private fun HttpStatus.wrapByHttpResponseMessageBuilderMock() = HttpResponseMessageBuilderMock().status(this)
 }
